d0863dea2e5d933aa7d735b0477e6cf948c7ed1d
ppamment <paul@byng-systems.com>
2017-03-24 12:05:32 +0000

    Minor refactor to remove anonymous function and slightly simplify code

--- src/MartinGeorgiev/Utils/DataStructure.php
+++ src/MartinGeorgiev/Utils/DataStructure.php
@@ -19,33 +19,29 @@ class DataStructure
      */
     public static function transformPostgresTextArrayToPHPArray($postgresArray)
     {
-        $transform = function ($textArrayToTransform) use (&$transform) {
-            $phpArray = str_getcsv(trim($textArrayToTransform, '{}'));
-            foreach ($phpArray as $i => $text) {
-                if ($text === null) {
-                    unset($phpArray[$i]);
-                    break;
-                }
-
-                $isInteger = is_numeric($text) && ''.intval($text) === $text;
-                if ($isInteger) {
-                    $phpArray[$i] = (int)$text;
-                    continue;
-                }
-
-                $isFloat = is_numeric($text) && ''.floatval($text) === $text;
-                if ($isFloat) {
-                    $phpArray[$i] = (float)$text;
-                    continue;
-                }
-
-                $phpArray[$i] = str_replace('\"', '"', $text);
+        $phpArray = str_getcsv(trim($postgresArray, '{}'));
+        foreach ($phpArray as $i => $text) {
+            if ($text === null) {
+                unset($phpArray[$i]);
+                break;
             }
 
-            return $phpArray;
-        };
+            $isInteger = is_numeric($text) && ''.intval($text) === $text;
+            if ($isInteger) {
+                $phpArray[$i] = (int)$text;
+                continue;
+            }
 
-        return $transform($postgresArray);
+            $isFloat = is_numeric($text) && ''.floatval($text) === $text;
+            if ($isFloat) {
+                $phpArray[$i] = (float)$text;
+                continue;
+            }
+
+            $phpArray[$i] = str_replace('\"', '"', $text);
+        }
+
+        return $phpArray;
     }
 
     /**
@@ -57,26 +53,22 @@ class DataStructure
      */
     public static function transformPHPArrayToPostgresTextArray(array $phpArray)
     {
-        $transform = function (array $phpArrayToTransform) use (&$transform) {
-            $result = [];
-            foreach ($phpArrayToTransform as $text) {
-                if (is_array($text)) {
-                    $result[] = $transform($text);
-                    continue;
-                }
-
-                if (is_numeric($text) || ctype_digit($text)) {
-                    $escapedText = $text;
-                } elseif (empty($text)) {
-                    $escapedText = '';
-                } else {
-                    $escapedText = '"' . str_replace('"', '\"', $text) . '"';
-                }
-                $result[] = $escapedText;
+        $result = [];
+        foreach ($phpArray as $text) {
+            if (is_array($text)) {
+                $result[] = self::transformPHPArrayToPostgresTextArray($text);
+                continue;
             }
-            return '{' . implode(",", $result) . '}';
-        };
-        
-        return $transform($phpArray);
+
+            if (is_numeric($text) || ctype_digit($text)) {
+                $escapedText = $text;
+            } elseif (empty($text)) {
+                $escapedText = '';
+            } else {
+                $escapedText = '"' . str_replace('"', '\"', $text) . '"';
+            }
+            $result[] = $escapedText;
+        }
+        return '{' . implode(",", $result) . '}';
     }
 }
